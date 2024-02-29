6f3ce393752b35f9f0fce8a68a87031a33b9e390
ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>
2024-02-29 14:02:12 +0530

    created pages
    Signed-off-by: ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import About from './pages/About'
 
 const App = () => {
   return (
-    <div className='text-3xl text-blue-400'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
