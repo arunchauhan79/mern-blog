afb3f80e13b56d0adccef6ac623586c0a4b9a73a
ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>
2024-03-13 11:09:10 +0530

    add create post api
    Signed-off-by: ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>

---
+++ api/models/post.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true,
+        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hostinger.co.uk%2Ftutorials%2Fhow-to-write-a-blog-post&psig=AOvVaw22jMxZMxYl9xhOetP4V2ON&ust=1710393895234000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMialfi_8IQDFQAAAAAdAAAAABAI"
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    category: {
+        type: String,
+        default: 'uncategorized'
+    }
+
+
+}, { timestamp: true });
+
+const Post = mongoose.model('Post', postSchema);
+export default Post;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import authRouter from './auth.route.js'
 import userRouter from './user.route.js'
+import postRouter from './post.route.js'
 
 
 export default Router().use('/auth', authRouter)
     .use('/user', userRouter)
+    .use('/post', postRouter)
