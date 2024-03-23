29cb83ccb11a873e7a7933d87ed40fb670bd9707
ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>
2024-03-24 00:36:51 +0530

    created post page
    Signed-off-by: ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
-import About from './pages/About'
+import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header';
 import FooterCom from './components/Footer'
-import PrivateRoute from './components/PrivateRoute'
-import CreatePost from './pages/CreatePost'
-import AdminPrivateRoute from './components/AdminPrivateRoute'
-import UpdatePost from './pages/UpdatePost'
+import AppRoutes from './AppRoutes';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route element={<PrivateRoute />}>
-          <Route path='/dashboard' element={<Dashboard />} />
-        </Route>
-        <Route element={<AdminPrivateRoute />}>
-          <Route path='/create-post' element={<CreatePost />} />
-          <Route path='/update-post/:postId' element={<UpdatePost />} />
-        </Route>
-        <Route path='/projects' element={<Projects />} />
-      </Routes>
+      <AppRoutes />
       <FooterCom />
     </BrowserRouter>
   )
---
+++ client/src/AppRoutes.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import PrivateRoute from './components/PrivateRoute'
+import AdminPrivateRoute from './components/AdminPrivateRoute'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import CreatePost from './pages/CreatePost'
+import UpdatePost from './pages/UpdatePost'
+import Projects from './pages/Projects'
+import PostPage from './pages/PostPage'
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route element={<PrivateRoute />}>
+                <Route path='/dashboard' element={<Dashboard />} />
+            </Route>
+            <Route element={<AdminPrivateRoute />}>
+                <Route path='/create-post' element={<CreatePost />} />
+                <Route path='/update-post/:postId' element={<UpdatePost />} />
+            </Route>
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/post/:slug' element={<PostPage />} />
+        </Routes>
+    )
+}
+
+export default AppRoutes
