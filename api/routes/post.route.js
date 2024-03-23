38447da8490c5e8596300cc549102ec82a559277
ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>
2024-03-23 12:33:55 +0530

    updated post completed
    Signed-off-by: ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { verifyUser } from './../utils/verifyUser.js';
-import { create, deletepost, getPosts } from '../controllers/post.controller.js';
+import { UpdatePost, create, deletepost, getPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyUser, create);
 router.get('/getPosts', getPosts)
 router.delete('/deletepost/:postId/:userId', verifyUser, deletepost)
+router.put('/updatePost/:postId/:userId', verifyUser, UpdatePost)
+
 
 export default router;
