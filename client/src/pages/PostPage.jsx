ae920a6c384167cf6f1e2f5f43b246f000cdc78d
ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>
2024-04-27 14:24:47 +0530

    add recent article section
    Signed-off-by: ArunChauhan <arun.chauhan@ukssgntis.onmicrosoft.com>

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
+import PostCard from '../components/PostCard';
 
 const PostPage = () => {
     const { slug } = useParams();
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
     const [post, setPost] = useState(null)
+    const [recentPosts, setRecentPosts] = useState(null)
     useEffect(() => {
 
         const fetchPost = async () => {
@@ -35,6 +37,24 @@ const PostPage = () => {
 
     }, [slug])
 
+    useEffect(() => {
+
+        const fetchRecentPosts = async () => {
+            try {
+                const res = await fetch('/api/post/getPosts?limit=3');
+                const data = await res.json();
+                if (res.ok) {
+                    setRecentPosts(data.posts)
+                }
+            }
+            catch (error) {
+                console.log(error.message)
+            }
+        }
+        fetchRecentPosts();
+    }, [])
+
+
     if (loading)
         return (
             <div className='flex justify-center items-center min-h-screen'>
@@ -59,6 +79,14 @@ const PostPage = () => {
             </div>
             <div>
                 <CommentSection postId={post._id} />
+                <div className='flex flex-col justify-center items-center mb-5'>
+                    <h1 className='text-xl mt-5'>Recent articles</h1>
+                    <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+                        {
+                            recentPosts && recentPosts.map(post => <PostCard key={post._id} post={post} />)
+                        }
+                    </div>
+                </div>
             </div>
         </main>
     )
